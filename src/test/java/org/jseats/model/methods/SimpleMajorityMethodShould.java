ae1b3f221a22f1aec906ceeb0a13f8342ba808eb
Alvaro Garcia <alvaro.garcia@scytl.com>
2015-01-23 12:20:40 +0100

    green: canary_test, fail_on_a_null_tally

---
+++ src/test/java/org/jseats/model/methods/SimpleMajorityMethodShould.java
@@ -0,0 +1,42 @@
+package org.jseats.model.methods;
+
+import org.hamcrest.MatcherAssert;
+import org.jseats.model.SeatAllocationException;
+import org.jseats.model.tie.TieBreaker;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import javax.lang.model.util.SimpleElementVisitor6;
+import java.util.Properties;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+public class SimpleMajorityMethodShould {
+    
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+    private SimpleMajorityMethod sut;
+
+    @Before
+    public void setUp(){
+        sut = new SimpleMajorityMethod();
+    }
+
+    @Test
+    public void canary_test() throws Exception {
+        assertTrue(true);
+    }
+
+    @Test
+    public void fail_on_a_null_tally() throws SeatAllocationException {
+        expectedException.expect(SeatAllocationException.class);
+        expectedException.expectMessage("Cannot process a null tallysheet");
+        
+        sut.process(null, mock(Properties.class), mock(TieBreaker.class));
+    }
+    
+}
