a2d9b75d4d2d703d4c86c702d12ae28a08c0ee73
Marc Miranda <marc.miranda@scytl.com>
2015-01-26 14:49:17 +0100

    Green: DHondtShould test

--- src/test/java/org/jseats/unit/DHondtShould.java
+++ src/test/java/org/jseats/unit/DHondtShould.java
@@ -30,7 +30,6 @@ import org.jseats.model.methods.DHondtHighestAveragesMethod;
 import org.jseats.model.tie.RandomTieBreaker;
 import org.jseats.model.tie.TieBreaker;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -38,20 +37,31 @@ import org.junit.rules.ExpectedException;
 public class DHondtShould {
 
 	private Tally tally;
+
 	private Properties properties;
+
 	private DHondtHighestAveragesMethod sut;
 
 	private static final String CANDIDATE_NAME_BOOZE = "Booze";
+
 	private static final String CANDIDATE_NAME_ROYALTY = "Royalty";
+
 	private static final String CANDIDATE_NAME_ROCK = "Rock";
+
 	private static final String CANDIDATE_NAME_POLITICS = "Politics";
 
 	private static final String CANDIDATE_NAME_red = "Red";
+
 	private static final String CANDIDATE_NAME_green = "Green";
+
 	private static final String CANDIDATE_NAME_blue = "Blue";
+
 	private static final String CANDIDATE_NAME_purple = "Purple";
+
 	private static final String CANDIDATE_NAME_black = "Black";
+
 	private static final String CANDIDATE_NAME_yellow = "Yellow";
+
 	private static final String CANDIDATE_NAME_brown = "Brown";
 
 	@Before
@@ -68,7 +78,7 @@ public class DHondtShould {
 	public void not_fail_on_a_null_tally() throws SeatAllocationException {
 		sut.process(null, mock(Properties.class), mock(TieBreaker.class));
 	}
-	
+
 	@Test
 	public void fail_when_no_candidates() throws SeatAllocationException {
 		expectedException.expect(SeatAllocationException.class);
@@ -77,9 +87,9 @@ public class DHondtShould {
 		doReturn("1").when(mockProperties).getProperty(anyString(), anyString());
 		sut.process(mock(Tally.class), mockProperties, null);
 	}
-	
+
 	@Test(expected = SeatAllocationException.class)
-	public void not_fail_on_null_properties() throws SeatAllocationException {
+	public void throw_an_exception_on_null_properties() throws SeatAllocationException {
 		sut.process(mock(Tally.class), null, mock(TieBreaker.class));
 	}
 
@@ -95,8 +105,7 @@ public class DHondtShould {
 	}
 
 	@Test
-	public void when_not_given_a_numberOfSeats_should_assign_the_same_number_as_numberOfCandidates()
-			throws SeatAllocationException {
+	public void when_not_given_a_numberOfSeats_should_assign_the_same_number_as_numberOfCandidates() throws SeatAllocationException {
 		Candidate candidateA = new Candidate("candidateA", 10);
 		RandomTieBreaker tieBreaker = new RandomTieBreaker();
 		tally.addCandidate(candidateA);
@@ -116,33 +125,39 @@ public class DHondtShould {
 	}
 
 	@Test
-	@Ignore
 	public void pass_the_acceptance_test_1() throws SeatAllocationException {
 		// Using test data set from US: https://redmine.scytl.net/issues/94064
 		properties.put(org.jseats.Properties.NUMBER_OF_SEATS, "5");
-		tally = getTallySheetWith(new Candidate(CANDIDATE_NAME_BOOZE, 40), new Candidate(CANDIDATE_NAME_ROYALTY, 70), new Candidate(CANDIDATE_NAME_ROCK,30), new Candidate(CANDIDATE_NAME_POLITICS,20));
+		tally =
+			getTallySheetWith(new Candidate(CANDIDATE_NAME_BOOZE, 40), new Candidate(CANDIDATE_NAME_ROYALTY, 70),
+				new Candidate(CANDIDATE_NAME_ROCK, 30), new Candidate(CANDIDATE_NAME_POLITICS, 20));
 		Result result = sut.process(tally, properties, new RandomTieBreaker());
+
 		assertEquals(result.getNumberOfSeatsForCandidate(CANDIDATE_NAME_BOOZE), 1);
-		assertEquals(result.getNumberOfSeatsForCandidate(CANDIDATE_NAME_ROCK), 1);
 		assertEquals(result.getNumberOfSeatsForCandidate(CANDIDATE_NAME_ROYALTY), 3);
+		assertEquals(result.getNumberOfSeatsForCandidate(CANDIDATE_NAME_ROCK), 1);
+		assertEquals(result.getNumberOfSeatsForCandidate(CANDIDATE_NAME_POLITICS), 0);
 	}
 
 	@Test
-	@Ignore
 	// Using test data set from http://icon.cat/util/elections is not possible
 	// due to limitations with online d'Hondt calculators and draw management
 	public void pass_the_acceptance_test_2() throws SeatAllocationException {
 		// Using test data set from http://icon.cat/util/elections
 		properties.put(org.jseats.Properties.NUMBER_OF_SEATS, "50");
-		tally = getTallySheetWith(new Candidate(CANDIDATE_NAME_brown, 1), new Candidate(CANDIDATE_NAME_yellow, 33), new Candidate(CANDIDATE_NAME_black, 22), new Candidate(CANDIDATE_NAME_purple, 12), new Candidate(CANDIDATE_NAME_blue, 75), new Candidate(CANDIDATE_NAME_green, 15), new Candidate(CANDIDATE_NAME_red, 50));
+		tally =
+			getTallySheetWith(new Candidate(CANDIDATE_NAME_brown, 1), new Candidate(CANDIDATE_NAME_yellow, 33),
+				new Candidate(CANDIDATE_NAME_black, 22), new Candidate(CANDIDATE_NAME_purple, 12), new Candidate(
+					CANDIDATE_NAME_blue, 75), new Candidate(CANDIDATE_NAME_green, 15),
+				new Candidate(CANDIDATE_NAME_red, 50));
 		Result result = sut.process(tally, properties, getRandomTieBreaker());
-		assertThat(result.getNumberOfSeatsForCandidate(CANDIDATE_NAME_red), is(4));
-		assertThat(result.getNumberOfSeatsForCandidate(CANDIDATE_NAME_green), is(0));
-		assertThat(result.getNumberOfSeatsForCandidate(CANDIDATE_NAME_blue), is(43));
-		assertThat(result.getNumberOfSeatsForCandidate(CANDIDATE_NAME_purple), is(0));
-		assertThat(result.getNumberOfSeatsForCandidate(CANDIDATE_NAME_black), is(1));
-		assertThat(result.getNumberOfSeatsForCandidate(CANDIDATE_NAME_yellow), is(2));
 		assertThat(result.getNumberOfSeatsForCandidate(CANDIDATE_NAME_brown), is(0));
+		assertThat(result.getNumberOfSeatsForCandidate(CANDIDATE_NAME_yellow), is(8));
+		assertThat(result.getNumberOfSeatsForCandidate(CANDIDATE_NAME_black), is(5));
+		assertThat(result.getNumberOfSeatsForCandidate(CANDIDATE_NAME_purple), is(3));
+		assertThat(result.getNumberOfSeatsForCandidate(CANDIDATE_NAME_blue), is(19));
+		assertThat(result.getNumberOfSeatsForCandidate(CANDIDATE_NAME_green), is(3));
+		assertThat(result.getNumberOfSeatsForCandidate(CANDIDATE_NAME_red), is(12));
 	}
 
 	private RandomTieBreaker getRandomTieBreaker() {
