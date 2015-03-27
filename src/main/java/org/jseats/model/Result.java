2347a8fc90548bf5f839cb4d2358d24784f430d8
Marc Miranda <marc.miranda@scytl.com>
2015-03-27 12:53:34 +0100

    Added ResultType.VOID

--- src/main/java/org/jseats/model/Result.java
+++ src/main/java/org/jseats/model/Result.java
@@ -27,7 +27,7 @@ public class Result {
 
 		// TODO AGB missing documentation
 		SINGLE("single-result"), MULTIPLE("multiple-result"), TIE("tie"), UNDECIDED(
-				"undecided");
+				"undecided"), VOID("void");
 
 		@SuppressWarnings("unused")
 		private final String type;
