8a72b4ed8cfd4228292e60bf0a1e68498b6a3db0
Leonardo Cappuccio <leonardo.cappuccio@scytl.com>
2015-04-09 18:14:40 +0200

    wrapper object for candidate

---
+++ src/main/java/org/jseats/model/Seat.java
@@ -0,0 +1,48 @@
+/**
+ * $Id$
+ *
+ * @author lcappuccio
+ * @date 09/04/2015 17:25
+ *
+ * Copyright (C) 2015 Scytl Secure Electronic Voting SA
+ *
+ * All rights reserved.
+ *
+ */
+package org.jseats.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Seat {
+	
+	private Candidate candidate;
+	@XmlAttribute
+	private int seatNumber;
+	
+	public Seat(Candidate candidate, int seatNumber) {
+		if (candidate == null) {
+			throw new IllegalArgumentException("Candidate cannot be null");
+		}
+		this.candidate = candidate;
+		this.seatNumber = seatNumber;
+	}
+
+	public Candidate getCandidate() {
+		return candidate;
+	}
+
+	public int getSeatNumber() {
+		return seatNumber;
+	}
+	
+	public int getVotes() {
+		return this.candidate.getVotes();
+	}
+
+	public String getName() {
+		return this.candidate.getName();
+	}
+}
