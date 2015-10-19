dbcece5f855b7d7455d3bf7b6c4a17cfa143af79
Oscar Ros <oscar.ros@scytl.com>
2015-10-19 13:11:07 +0200

    User Story #6285: Implement an extended D'Hondt strategy in JSeats
    - refactor: prefer basing the divisor on the round number rather than a separate value

--- src/main/java/org/jseats/model/methods/DHondtExtendedMethod.java
+++ src/main/java/org/jseats/model/methods/DHondtExtendedMethod.java
@@ -139,19 +139,16 @@ public class DHondtExtendedMethod extends DHondtHighestAveragesMethod {
 
 	private double[][] calculateQuotientsPerRound(InmutableTally tally, int numberOfCandidates, int numberOfSeats) {
 		double[][] result = new double[numberOfCandidates][numberOfSeats];
-		double divisor = 1;
 
-		for (int round = 0; round < numberOfSeats; round++) {
-
+		for (int round = 0; round < numberOfSeats; round++){
+			double divisor = round + 1;
 			log.debug(round + " / " + divisor + " : ");
 
 			for (int candidate = 0; candidate < numberOfCandidates; candidate++) {
 				result[candidate][round] = (tally.getCandidateAt(candidate).getVotes() / divisor);
 				log.debug(String.format("%.2f", result[candidate][round]) + ",\t");
 			}
-			divisor++;
 		}
-
 		return result;
 	}
 
